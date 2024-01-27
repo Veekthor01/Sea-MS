b421c2ae89199875c50d919d56213d8c74ffa042
Veekthor01 <veekthor01@gmail.com>
2024-01-27 22:36:11 +0300

    Refactor server.ts: Update imports, add passport authentication, and configure routes

--- Server/server.ts
+++ Server/server.ts
@@ -1,12 +1,21 @@
-const express = require('express');
-const morgan = require('morgan');
-const cors = require('cors');
-const helmet = require('helmet');
-require('dotenv').config();
+import express from 'express';
+import morgan from 'morgan';
+import cors from 'cors';
+import helmet from 'helmet';
+import dotenv from 'dotenv';
+import passport from 'passport';
+import './Passport-Config/passport';
+import connectDB from './DB/db';
+import signupRouter from './Routes/signup';
+import loginRouter from './Routes/login';
+
+dotenv.config();
 
 const app = express();
 
 const PORT = process.env.PORT || 5000;
+
+connectDB();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -22,6 +31,11 @@ app.use(helmet.contentSecurityPolicy({
         scriptSrc: ["'self'"],
     }
 }));
+app.use(passport.initialize());
+
+app.use('/signup', signupRouter);
+app.use('/login', loginRouter);
+
 
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
