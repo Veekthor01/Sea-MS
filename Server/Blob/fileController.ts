fd9608d016710d922fcd5c52de155dda9630e340
Veekthor01 <veekthor01@gmail.com>
2024-01-30 22:05:55 +0300

    Add fileController for managing files in Google Cloud Storage

---
+++ Server/Blob/fileController.ts
@@ -0,0 +1,64 @@
+import { Storage } from '@google-cloud/storage';
+import dotenv from 'dotenv';
+dotenv.config();
+
+const bucketName = process.env.GOOGLE_STORAGE_BUCKET_NAME;
+const googleCloudKey = process.env.GOOGLE_CLOUD_KEY;
+
+// Instantiate a storage client with credentials
+const storage = new Storage({ keyFilename: googleCloudKey });
+
+const getListFiles = async (req: any, res: any) => {
+    try {
+        if (!googleCloudKey || !bucketName) {
+            throw new Error('Environment variables are not set');
+        }
+    const [files] = await storage.bucket(bucketName).getFiles();
+    let fileInfos: any = [];
+
+    files.forEach((file) => {
+      fileInfos.push({
+        name: file.name,
+        url: file.metadata.mediaLink,
+      });
+    });
+    res.status(200).send(fileInfos);
+    } catch (err: any) {
+        console.log(err);
+        res.status(500).json({ message: err });
+    }
+};
+
+const downloadFile = async (req: any, res: any) => {
+    try {
+        if (!googleCloudKey || !bucketName) {
+            throw new Error('Environment variables are not set');
+        }
+        const fileName = req.params.name;
+        const file = storage.bucket(bucketName).file(fileName);
+        const [metadata] = await file.getMetadata();
+        res.redirect(metadata.mediaLink);
+    
+    } catch (err: any) {
+        console.log(err);
+        res.status(500).json({ message:'Could not download the file.', error: err});
+    }
+}
+
+const deleteFileFromStorage = async (publicUrl: string) => {
+    try {
+        if (!googleCloudKey || !bucketName) {
+            throw new Error('Environment variables are not set');
+        }
+        const fileName = publicUrl.split('/').pop();
+        if (!fileName) {
+            throw new Error('File name not found');
+        }
+        await storage.bucket(bucketName).file(fileName).delete();
+    } catch (err) {
+        console.log(err);
+        throw err;
+    }
+}
+
+export { getListFiles, downloadFile, deleteFileFromStorage};
