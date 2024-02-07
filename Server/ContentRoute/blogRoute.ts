eb132ccaf2e2b54f5850dfd4715b690580cfc9b4
Veekthor01 <veekthor01@gmail.com>
2024-02-07 16:13:45 +0300

    Refactor date display in blogRoute.ts

--- Server/ContentRoute/blogRoute.ts
+++ Server/ContentRoute/blogRoute.ts
@@ -1,5 +1,6 @@
 import express from 'express';
 import { ObjectId } from 'mongodb';
+import { format } from 'date-fns';
 //import { createBlog, getBlogByName,  getBlogById ,getAllBlogs ,updateBlog, deleteBlog } from '../Content/blog';
 import { getBlogTemplateByName, saveUserBlog, getUserBlogByName, getUserBlogById, getAllUserBlogs, updateUserBlog, deleteUserBlog } from '../Template/blogTemplate';
 const router = express.Router();
@@ -42,9 +43,13 @@ router.get('/:name', async (req, res) => {
         const blog = await getUserBlogByName(req.params.name);
         if (blog) {
             if (blog.createdAt && blog.updatedAt) {
-            const displayDate = blog.createdAt.getTime() === blog.updatedAt.getTime() ? blog.createdAt : blog.updatedAt;
-            console.log('Display date:', displayDate);
-        }}
+                const createdAt = new Date(blog.createdAt);
+                const updatedAt = new Date(blog.updatedAt);
+                const displayDate = createdAt.getTime() === updatedAt.getTime() ? createdAt : updatedAt;
+                const formattedDisplayDate = format(displayDate, 'dd MMM yyyy');
+                console.log('Display date:', formattedDisplayDate);
+            }
+        }
         res.status(200).json(blog);
     } catch (err) {
         res.status(500).json({ message: 'Failed to retrieve blog post' });
@@ -58,9 +63,13 @@ router.get('/id/:id', async (req, res) => {
         const blog = await getUserBlogById(_id);
         if (blog) {
             if (blog.createdAt && blog.updatedAt) {
-            const displayDate = blog.createdAt.getTime() === blog.updatedAt.getTime() ? blog.createdAt : blog.updatedAt;
-            console.log('Display date:', displayDate);
-        }}
+                const createdAt = new Date(blog.createdAt);
+                const updatedAt = new Date(blog.updatedAt);
+                const displayDate = createdAt.getTime() === updatedAt.getTime() ? createdAt : updatedAt;
+                const formattedDisplayDate = format(displayDate, 'dd MMM yyyy');
+                console.log('Display date:', formattedDisplayDate);
+            }
+        }
         res.status(200).json(blog);
     } catch (err) {
         res.status(500).json({ message: 'Failed to retrieve blog post' });
