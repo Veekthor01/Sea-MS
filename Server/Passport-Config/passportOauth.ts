f7b45b1c4233e9f5db443a251c6a5d1fde4238db
Veekthor01 <veekthor01@gmail.com>
2024-02-10 13:37:45 +0300

    Add JWT token generation and refresh token storage

--- Server/Passport-Config/passportOauth.ts
+++ Server/Passport-Config/passportOauth.ts
@@ -1,12 +1,16 @@
 import GoogleStrategy from 'passport-google-oauth20';
+import jwt from 'jsonwebtoken';
 import passport from 'passport';
 import dotenv from 'dotenv';
 dotenv.config();
 import { createUser, getUserByGoogleId } from '../DB/user';
+import { storeRefreshToken } from '../DB/user';
 
 const GOOGLE_CLIENT_ID = process.env.GOOGLE_CLIENT_ID;
 const GOOGLE_CLIENT_SECRET = process.env.GOOGLE_CLIENT_SECRET;
 const BACKEND_URL = process.env.BACKEND_URL;
+const JWT_SECRET = process.env.JWT_SECRET;
+const JWT_REFRESH = process.env.JWT_REFRESH;
 
 // Add check to ensure that the environment variables are set
 if (!GOOGLE_CLIENT_ID || !GOOGLE_CLIENT_SECRET || !BACKEND_URL) {
@@ -32,6 +36,17 @@ passport.use(
             const createdUser = await createUser(newUser);
             return done(null, createdUser);
         }
+        // Generate the token for the user
+        const token = jwt.sign({ _id: user._id }, JWT_SECRET as string, { expiresIn: '1h' });
+        const refreshToken = jwt.sign({ _id: user._id }, JWT_REFRESH as string, { expiresIn: '30d' });
+         // Store the refresh token in the database
+        if (user._id) {
+            await storeRefreshToken(refreshToken, user._id.toString());
+        } else {
+            // Handle the case where user._id is undefined
+            console.error('User ID is undefined');
+        }
+        user.token = token;
         return done(null, user);
     } catch (err) {
         return done(err, false);
