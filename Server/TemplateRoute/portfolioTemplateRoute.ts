b0b19f6d460979aa1ae9584b43146ae5ef9b05b9
Veekthor01 <veekthor01@gmail.com>
2024-02-11 15:47:41 +0300

    Add JWT authentication to portfolio template routes

--- Server/TemplateRoute/portfolioTemplateRoute.ts
+++ Server/TemplateRoute/portfolioTemplateRoute.ts
@@ -1,11 +1,12 @@
 import express from 'express';
 import { ObjectId } from 'mongodb';
 import { createPortfolioTemplate, getPortfolioTemplateByName, getAllPortfolioTemplates, getPortfolioTemplateById, updatePortfolioTemplate, deletePortfolioTemplate } from '../Template/portfolioTemplate';
+import authenticateJWT from '../Passport-Config/auth';
 
 const router = express.Router();
 
 // Route to create a portfolio template
-router.post('/', async (req, res) => {
+router.post('/', authenticateJWT, async (req, res) => {
     try {
         const portfolioTemplate = await createPortfolioTemplate(req.body);
         res.status(200).json(portfolioTemplate);
@@ -15,7 +16,7 @@ router.post('/', async (req, res) => {
 });
 
 //Route to get all portfolio templates
-router.get('/', async (req, res) => {
+router.get('/', authenticateJWT, async (req, res) => {
     try {
         const portfolioTemplates = await getAllPortfolioTemplates();
         res.status(200).json(portfolioTemplates);
@@ -25,7 +26,7 @@ router.get('/', async (req, res) => {
 });
 
 // Route to get a portfolio template by name
-router.get('/:name', async (req, res) => {
+router.get('/:name', authenticateJWT, async (req, res) => {
     try {
         const portfolioTemplate = await getPortfolioTemplateByName(req.params.name);
         res.status(200).json(portfolioTemplate);
@@ -35,7 +36,7 @@ router.get('/:name', async (req, res) => {
 });
 
 // Route to get a portfolio template by id
-router.get('/id/:id', async (req, res) => {
+router.get('/id/:id', authenticateJWT, async (req, res) => {
     try {
         const _id = new ObjectId(req.params.id);
         const portfolioTemplate = await getPortfolioTemplateById(_id);
@@ -46,7 +47,7 @@ router.get('/id/:id', async (req, res) => {
 });
 
 // Route to update a portfolio template by id
-router.put('/:id', async (req, res) => {
+router.put('/:id', authenticateJWT, async (req, res) => {
     try {
         const _id = new ObjectId(req.params.id);
         const portfolioTemplate = await updatePortfolioTemplate(_id, req.body);
@@ -57,7 +58,7 @@ router.put('/:id', async (req, res) => {
 });
 
 // Route to delete a portfolio template by id
-router.delete('/:id', async (req, res) => {
+router.delete('/:id', authenticateJWT, async (req, res) => {
     try {
         const _id = new ObjectId(req.params.id);
         const result = await deletePortfolioTemplate(_id);
