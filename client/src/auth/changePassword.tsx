c02618de1be3a4641f7c6f4817e7f877c746a0c0
Veekthor01 <veekthor01@gmail.com>
2024-02-17 10:44:43 +0300

    Refactor change password form UI

--- client/src/auth/changePassword.tsx
+++ client/src/auth/changePassword.tsx
@@ -32,18 +32,44 @@ function ChangePasswordForm() {
 
   return (
     <>
-    <CheckAuthenticated />
-    <form onSubmit={handleSubmit}>
-      <label>
-        Old Password:
-        <input type="password" value={oldPassword} onChange={e => setOldPassword(e.target.value)} required />
-      </label>
-      <label>
-        New Password:
-        <input type="password" value={newPassword} onChange={e => setNewPassword(e.target.value)} required />
-      </label>
-      <button type="submit">Change Password</button>
-    </form>
+      <CheckAuthenticated />
+      <section className='bg-zinc-100'>
+        <div className='flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0'>
+        <h1 className='mb-6 text-center text-xl md:text-2xl text-zinc-900 font-roboto font-bold leading-normal tracking-wide'>
+            Change Your Password
+            </h1>
+          <form onSubmit={handleSubmit} className='w-full bg-white rounded-lg shadow md:mt-0 sm:max-w-md xl:p-0'>
+            <div className='p-6 space-y-4 md:space-y-6 sm:p-8'>
+              <div>
+              <label className='block mb-2 text-sm font-roboto font-bold leading-normal tracking-wide'>
+                  Old Password
+                  </label>
+                <input type="password" 
+                  value={oldPassword} 
+                  onChange={e => setOldPassword(e.target.value)} 
+                  className='bg-zinc-50 border border-zinc-300 text-zinc-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2 md:p-2.5'
+                  required 
+                />
+              </div>
+              <div>
+              <label className='block mb-2 text-sm font-roboto font-bold leading-normal tracking-wide'>
+                  New Password
+                  </label>
+                <input type="password" 
+                  value={newPassword} 
+                  onChange={e => setNewPassword(e.target.value)} 
+                  className='bg-zinc-50 border border-zinc-300 text-zinc-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2 md:p-2.5'
+                  required 
+                />
+              </div>
+              <button type="submit" 
+           className='w-full font-roboto text-zinc-100 bg-zinc-900 hover:bg-zinc-800 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-zinc-500 font-medium rounded-lg text-sm px-5 py-2.5 text-center leading-normal tracking-wide'>
+                Change Password
+              </button>
+            </div>
+          </form>
+        </div>
+      </section>
     </>
   );
 }
